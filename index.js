c93c8e39ab9be6e04cf534a75147f68f264b0e28
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-03-07 22:16:36 +0000

    updated function name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const functions = require('@google-cloud/functions-framework');
 
-functions.http('helloHttp', (req, res) => {
+functions.http('corso-function', (req, res) => {
   res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
 });
