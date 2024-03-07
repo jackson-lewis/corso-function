9afe8460adf9b5ef28f769eef403063284058fef
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-03-07 23:03:27 +0000

    updated function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('helloWorld', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+  res.send(`Hello to you ${req.query.name || req.body.name || 'World'}!`);
 });
