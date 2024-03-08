b825ab7163854e4dda50898e9dc4daa525aebdc5
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-03-08 16:20:30 +0000

    updated function

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { http } from '@google-cloud/functions-framework'
 
 http('helloWorld', (req, res) => {
-    res.send(`Hello world to you ${req.query.name}`)
+    res.send(`Hello world ${req.query.name}`)
 })
