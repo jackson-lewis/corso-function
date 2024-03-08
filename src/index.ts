ad76bc1325e2053018946b878356e55be1d89714
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-03-08 15:26:08 +0000

    Updated files sent to gcloud

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { http } from '@google-cloud/functions-framework'
 
-http('helloWorld', function (req, res) {
+http('helloWorld', (req, res) => {
     res.send(`Hello world to you ${req.query.name}`)
 })
