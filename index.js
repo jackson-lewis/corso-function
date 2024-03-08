bedf1f56f15ccea2f2fe716807fc39585aeea666
Jackson Lewis <hello@jacksonlewis.co.uk>
2024-03-08 15:21:56 +0000

    Added TypeScript

--- index.js
+++
@@ -1,5 +0,0 @@
-const functions = require('@google-cloud/functions-framework');
-
-functions.http('helloWorld', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
-});
